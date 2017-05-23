d4d4e2524d91764fef5e72f15196248508367acf
EeeasyBreezy <ag1011@yandex.ru>
2017-05-23 18:07:01 +0300

    Прототип чекера номеров в телеге 1. Подключение к апи 2. Проверка номера телефона на валидность

---
+++ TLPhoneChecker/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using TLSharp.Core;
+
+namespace TLPhoneChecker
+{
+    internal class Program
+    {
+        private static int API_ID = 129062;
+        private static string API_HASH = "7bb45a0b8bea42bcd20e6750e1f9c8d8";
+        private static FileSessionStore store = null;
+        private static TelegramClient client = null;
+        private static async void LaunchChecker()
+        {
+
+            var reader = new StreamReader("phones.txt");
+
+
+            await client.ConnectAsync();
+            while (client.IsConnected == false)
+            {
+            }
+            while (reader.EndOfStream == false)
+            {
+                var number = reader.ReadLine();
+                bool isRegistered = await client.IsPhoneRegisteredAsync(number);
+                Console.WriteLine("{0} : {1}", number, isRegistered.ToString());
+                Thread.Sleep(500);
+            }
+        }
+
+        private static async Task ConnectToAPI()
+        {
+            await client.ConnectAsync();
+        }
+
+        private static async Task CheckPhone(string phone)
+        {
+            bool result = await client.IsPhoneRegisteredAsync(phone);
+            Console.WriteLine("{0}, {1}", phone, result.ToString());
+        }
+        private static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("phones.txt");
+
+            store = new FileSessionStore();
+            client = new TelegramClient(API_ID, API_HASH, store, "session");
+            ConnectToAPI().Wait();
+            while (reader.EndOfStream == false)
+            {
+                string number = reader.ReadLine();
+                CheckPhone(number).Wait();
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+
+        }
+    }
+}
